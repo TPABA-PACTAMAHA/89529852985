3db9f9f6d6008f5f465a1d2f1b55d64aefd9e50a
TPABA-PACTAMAHA <157577595+TPABA-PACTAMAHA@users.noreply.github.com>
2024-01-24 11:18:06 +0300

    файл

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and pre"Run" button to execute it.
+
+'''
+
+print("Hello World")
+#coding:utf-8
+print(привет мир меня зовут масло)#dsdjlbn ghbdtncndbt yf 'rhfy'
